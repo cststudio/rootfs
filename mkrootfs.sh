0897ea3ec9bf294c30fae8051cf15b6555127c6a
Late Lee <li@latelee.org>
2019-05-03 16:27:29 +0800

    更新库，增大镜像大小为64MB、
    Signed-off-by: Late Lee <li@latelee.org>

--- mkrootfs.sh
+++ mkrootfs.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-ROOTFS=rootfs_imx
+ROOTFS=rootfs
 MOUNT_DIR=/tmp/tmpfs
 IMG=rootfs.img
+SIZE=64
 
-dd if=/dev/zero of=$IMG bs=1M count=$((32))
+dd if=/dev/zero of=$IMG bs=1M count=$SIZE
 mkfs.ext3 $IMG
 mkdir -p $MOUNT_DIR
 mount -t ext3 $IMG $MOUNT_DIR -o loop
